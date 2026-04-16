16e40662a57be39b612a20802f1c39090419655a
Andriy Gavrylenko <gavrosh2007@gmail.com>
2026-04-16 13:46:24 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'dualtoken-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './offline.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  './01.jpeg',
+  './02.jpeg',
+  './03.jpeg',
+  './04.jpeg',
+  './05.jpeg',
+  './06.jpeg',
+  './07.jpg',
+  './08.jpeg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('./offline.html');
+          }
+          return new Response('Offline content not available', {
+            status: 503,
+            statusText: 'Service Unavailable'
+          });
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    )).then(() => self.clients.claim())
+  );
+});
